1ba8660d041e300adaf8ff8bf36d7ec3768fbca3
Paul Mooney <paul.mooney@hmhco.com>
2014-05-06 18:29:55 +0100

    Added performance specs.

---
+++ Daishi.Microservices.Components.Serialisation.Specs/SpeedComparison.feature
@@ -0,0 +1,9 @@
+﻿Feature: SpeedComparison
+	In order to prove that these serialisation components are faster than JSON.NET
+	As a speed-test
+	I want to compare these components speed with JSON.NET
+
+Scenario: Compare speed
+	Given I have parsed a JSON object using these components
+	And I have parsed a JSON object using JSON.NET	
+	Then the response value associated with these components should be the lowest value
---
+++ Daishi.Microservices.Components.Serialisation.Specs/SpeedComparisonSteps.cs
@@ -0,0 +1,55 @@
+﻿#region Includes
+
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+#endregion
+
+namespace Daishi.Microservices.Components.Serialisation.Specs {
+    [Binding]
+    public class SpeedComparisonSteps {
+        private Stopwatch _stopwatch;
+        private long _result1, _result2;
+
+        [Given(@"I have parsed a JSON object using these components")]
+        public void GivenIHaveParsedAJSONObjectUsingTheseComponents() {
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(Resources.LargeJSON));
+            var reader = new BinaryReader(stream);
+            var finder = new JsonPropertyFinder(reader, new WordBuilder(reader),
+                new JsonPropertyValidator(reader));
+
+            finder.Find("response");
+            var jsonObjectBuilder = new JsonObjectBuilder(
+                reader, new JsonContainerFactory(), new JsonReader(reader));
+
+            var response = string.Concat("response:", jsonObjectBuilder.Build(JsonObjectType.Object));
+
+            _stopwatch.Stop();
+            _result1 = _stopwatch.ElapsedMilliseconds;
+        }
+
+        [Given(@"I have parsed a JSON object using JSON\.NET")]
+        public void GivenIHaveParsedAJSONObjectUsingJSON_NET() {
+            _stopwatch.Start();
+
+            var jsonObject = JObject.Parse(Resources.LargeJSON);
+            var response = jsonObject["response"].ToString(Formatting.None);
+
+            _stopwatch.Stop();
+            _result2 = _stopwatch.ElapsedMilliseconds;
+        }
+
+        [Then(@"the response value associated with these components should be the lowest value")]
+        public void ThenTheResponseValueAssociatedWithTheseComponentsShouldBeTheLowestValue() {
+            Assert.Greater(_result2, _result1);
+        }
+    }
+}
