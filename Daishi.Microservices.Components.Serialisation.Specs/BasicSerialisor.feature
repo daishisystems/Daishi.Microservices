6a72c05d92aa9ac68076eb862d9920dd49d94b04
Paul Mooney <paul.mooney@hmhco.com>
2014-05-13 14:01:56 +0100

    Added BasicSerialisor spec.

---
+++ Daishi.Microservices.Components.Serialisation.Specs/BasicSerialisor.feature
@@ -0,0 +1,10 @@
+﻿Feature: BasicSerialisor
+	In order to ensure that simple objects can be serialised
+	As a BasicSerialisor
+	I want to serialise a simple object
+
+Scenario: Serialise a simple object
+	Given I have supplied a simple object
+	And I have instantiated a BasicSerialisor
+	When I serialise the simple object
+	Then the simple object should be serialised correctly
---
+++ Daishi.Microservices.Components.Serialisation.Specs/BasicSerialisorSteps.cs
@@ -0,0 +1,30 @@
+﻿#region Includes
+
+using TechTalk.SpecFlow;
+
+#endregion
+
+namespace Daishi.Microservices.Components.Serialisation.Specs {
+    [Binding]
+    public class BasicSerialisorSteps {
+        [Given(@"I have supplied a simple object")]
+        public void GivenIHaveSuppliedASimpleObject() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"I have instantiated a BasicSerialisor")]
+        public void GivenIHaveInstantiatedABasicSerialisor() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I serialise the simple object")]
+        public void WhenISerialiseTheSimpleObject() {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the simple object should be serialised correctly")]
+        public void ThenTheSimpleObjectShouldBeSerialisedCorrectly() {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
